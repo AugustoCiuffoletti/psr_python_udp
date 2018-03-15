2d1c8b951d18a7001ddbd94b226464d8f16608be
Augusto Ciuffoletti <augusto@di.unipi.it>
2018-03-15 11:21:21 +0100

    local->remote

--- source.py
+++ source.py
@@ -1,8 +1,8 @@
 import socket, sys
 if len(sys.argv) != 3:                                        # Controllo parametri
-  print('\nUsage: '+sys.argv[0]+' <local_IP> <local_port\n')
+  print('\nUsage: '+sys.argv[0]+' <remote_IP> <remote_port\n')
   exit(1)
-local_IP=sys.argv[1]                                          # Acquisizione parametri
-local_Port=int(sys.argv[2])
+remote_IP=sys.argv[1]                                          # Acquisizione parametri
+remote_IP_Port=int(sys.argv[2])
 source = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)     # Creazione socket
-source.sendto("Hallo", (local_IP,local_Port))                 # Invio
+source.sendto("Hallo", (remote_IP,remote_Port))                 # Invio
