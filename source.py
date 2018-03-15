8b24092dcf01516ec837e1efa030cbe6a0375e1d
Augusto Ciuffoletti <augusto@di.unipi.it>
2018-03-15 08:53:13 +0100

    linea di comando e controllo

--- source.py
+++ source.py
@@ -1,11 +1,8 @@
 import socket, sys
-
-if len(sys.argv) != 3:            # Controllo parametri
+if len(sys.argv) != 3:                                        # Controllo parametri
   print('\nUsage: '+sys.argv[0]+' <local_IP> <local_port\n')
   exit(1)
-local_IP=sys.argv[1]
+local_IP=sys.argv[1]                                          # Acquisizione parametri
 local_Port=int(sys.argv[2])
-
-Message = "Hello"
-source = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-source.sendto("Hallo", (local_IP,local_Port))
+source = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)     # Creazione socket
+source.sendto("Hallo", (local_IP,local_Port))                 # Invio
